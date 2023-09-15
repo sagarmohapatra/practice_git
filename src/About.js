5f9e9c05794ca54459a785a115981fa27cec2716
sagarmohapatra <79126812+sagarmohapatra@users.noreply.github.com>
2023-09-16 00:05:25 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ function About() {
   return (
     <div>
         <div>i am from about</div>
-        <div>i am from any part</d>
+        <div>i am from any part</div>
+    <div>dfghjk</div>
     </div>
   )
 }

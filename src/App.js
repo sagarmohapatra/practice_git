1706074e70b486d8be28d91c8fe5f36e557b1c40
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-09-15 23:36:05 +0530

    second_data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import About from './About';
 function App() {
   return (
     <div className="App">
      <Home/>
+     <About/>
     </div>
   );
 }
